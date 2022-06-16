874efafff50f421b3eb2a9e925ae5fa903b1e016
RohitChugwani <rchugwani3@gmail.com>
2022-06-16 12:25:24 +0530

    Welcome to master branch

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to address book system");
+    }
+}
